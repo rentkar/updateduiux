a48f15b19d8f76c0bcddba40e1fe5b177edbed1f
Rentkar <rentkarr@gmail.com>
2021-03-02 12:23:08 +0530

    Updates

---
+++ precache-manifest.bd1b7271faf954c9bc7637507925916a.js
@@ -0,0 +1,514 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0234a36a338cdb352b72477d9bd65a79",
+    "url": "/updateduiux/index.html"
+  },
+  {
+    "revision": "e1b0247d5d75140dc489",
+    "url": "/updateduiux/static/css/2.03989b6f.chunk.css"
+  },
+  {
+    "revision": "dcda82db2bfc50fcdd01",
+    "url": "/updateduiux/static/css/main.9f0b8563.chunk.css"
+  },
+  {
+    "revision": "e1b0247d5d75140dc489",
+    "url": "/updateduiux/static/js/2.363bd7fb.chunk.js"
+  },
+  {
+    "revision": "04625bd1a4f9c05ed205f651693d4996",
+    "url": "/updateduiux/static/js/2.363bd7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcda82db2bfc50fcdd01",
+    "url": "/updateduiux/static/js/main.8015cb09.chunk.js"
+  },
+  {
+    "revision": "c469ef6ee78652de644a",
+    "url": "/updateduiux/static/js/runtime-main.e628a393.js"
+  },
+  {
+    "revision": "2f4c116cc3844a03280a0fb26973c5fa",
+    "url": "/updateduiux/static/media/Console.2f4c116c.png"
+  },
+  {
+    "revision": "cf618bc4b9942ee829383c46e0d1ed21",
+    "url": "/updateduiux/static/media/FIFA.cf618bc4.png"
+  },
+  {
+    "revision": "c5bb0659762a6eedb8d0ae4d00fd4a94",
+    "url": "/updateduiux/static/media/GUITAR GRADIENT.c5bb0659.png"
+  },
+  {
+    "revision": "515fa585bb4522c0471bc3d4d97532d3",
+    "url": "/updateduiux/static/media/GUITAR GREY.515fa585.png"
+  },
+  {
+    "revision": "d025b84f4efbbbd27569ee8a7a9748f5",
+    "url": "/updateduiux/static/media/PuneC.d025b84f.png"
+  },
+  {
+    "revision": "e82057a01c6e61b7cbc4083fdfd14973",
+    "url": "/updateduiux/static/media/PuneCG.e82057a0.png"
+  },
+  {
+    "revision": "f3ea48515dc4472afda75f4efe994788",
+    "url": "/updateduiux/static/media/aboutf.f3ea4851.png"
+  },
+  {
+    "revision": "e426f6e0e5cdb0895c106ddd345f6949",
+    "url": "/updateduiux/static/media/abouto.e426f6e0.png"
+  },
+  {
+    "revision": "7e9683469442ba5045151c675d2717e3",
+    "url": "/updateduiux/static/media/accesories colour.7e968346.png"
+  },
+  {
+    "revision": "c407dbca16c259f7387a0a63a9a55d20",
+    "url": "/updateduiux/static/media/accesories grey.c407dbca.png"
+  },
+  {
+    "revision": "37ac9c9004f7ad88b4b701bc79982ed9",
+    "url": "/updateduiux/static/media/amplifier colour.37ac9c90.png"
+  },
+  {
+    "revision": "e8c31640e3f67ad939510e31202e3808",
+    "url": "/updateduiux/static/media/amplifier grey.e8c31640.png"
+  },
+  {
+    "revision": "37193046f1093798947f35423c0800f1",
+    "url": "/updateduiux/static/media/bag+.37193046.png"
+  },
+  {
+    "revision": "75bed337b5f5c66de263dc297c3b9b5e",
+    "url": "/updateduiux/static/media/bag.75bed337.png"
+  },
+  {
+    "revision": "90658c4202fb34b700c0eb7a3813a687",
+    "url": "/updateduiux/static/media/bagb.90658c42.png"
+  },
+  {
+    "revision": "426c5c9529b6b353e73d2c23665ee4d3",
+    "url": "/updateduiux/static/media/bagf.426c5c95.png"
+  },
+  {
+    "revision": "87620698d7b2b0f39d4b2573988f369d",
+    "url": "/updateduiux/static/media/bago.87620698.png"
+  },
+  {
+    "revision": "602880709a37aeefc5c71427f8a0aefb",
+    "url": "/updateduiux/static/media/battlefield.60288070.png"
+  },
+  {
+    "revision": "123c5a903a0ff498f4ffeaaaab49fb98",
+    "url": "/updateduiux/static/media/camera colour.123c5a90.png"
+  },
+  {
+    "revision": "d5e6b9d7ac610abdcded011eb9d85eeb",
+    "url": "/updateduiux/static/media/camera grey.d5e6b9d7.png"
+  },
+  {
+    "revision": "adf0e19fb478f28dce586cabf6ba16d4",
+    "url": "/updateduiux/static/media/card.adf0e19f.png"
+  },
+  {
+    "revision": "94d8b7047d5fb826fcb61bfb20041e6e",
+    "url": "/updateduiux/static/media/category1.94d8b704.png"
+  },
+  {
+    "revision": "037b90886bb502335e68d17324bfde0a",
+    "url": "/updateduiux/static/media/category2.037b9088.png"
+  },
+  {
+    "revision": "bd86793897e79fce71927bf7a6e78ca5",
+    "url": "/updateduiux/static/media/category3.bd867938.png"
+  },
+  {
+    "revision": "0b998e94c969ad0cb3f07969c8efed6d",
+    "url": "/updateduiux/static/media/category4.0b998e94.png"
+  },
+  {
+    "revision": "f4ef82768b63dcb47a63775bcfada077",
+    "url": "/updateduiux/static/media/catogories-40.f4ef8276.png"
+  },
+  {
+    "revision": "7315f0e8221e02360ca9ff1b5ff428d6",
+    "url": "/updateduiux/static/media/catogories-49.7315f0e8.png"
+  },
+  {
+    "revision": "822137416ed223d3e977f67dcd0e8112",
+    "url": "/updateduiux/static/media/changes_icon-01.82213741.png"
+  },
+  {
+    "revision": "5d492adbc491b9ef26b6bcfc525c177e",
+    "url": "/updateduiux/static/media/changes_icon-03.5d492adb.png"
+  },
+  {
+    "revision": "56fcec82e0e54d89609d0c45c5227573",
+    "url": "/updateduiux/static/media/console gradient.56fcec82.png"
+  },
+  {
+    "revision": "989b28a69c443bf2331f010712410d2d",
+    "url": "/updateduiux/static/media/console grey.989b28a6.png"
+  },
+  {
+    "revision": "650569aba792cc9fbe92724ee22b6ad9",
+    "url": "/updateduiux/static/media/controller.650569ab.png"
+  },
+  {
+    "revision": "7f6216560d825dfd039bc51b626fd05a",
+    "url": "/updateduiux/static/media/covid sefty web-03.7f621656.png"
+  },
+  {
+    "revision": "61c8a755ba47d93ef91174abb37bd500",
+    "url": "/updateduiux/static/media/covid sefty web-04.61c8a755.png"
+  },
+  {
+    "revision": "da6cce61228a205b22841de8504ef0ff",
+    "url": "/updateduiux/static/media/covid.da6cce61.png"
+  },
+  {
+    "revision": "0dea09a40e34635251376313a7cce9d9",
+    "url": "/updateduiux/static/media/cust1.0dea09a4.png"
+  },
+  {
+    "revision": "4337d0638c15aeca32372c864b8a1459",
+    "url": "/updateduiux/static/media/cust2.4337d063.png"
+  },
+  {
+    "revision": "9ff09a592ada199cb8a072bfb6af417b",
+    "url": "/updateduiux/static/media/dummyprofile.9ff09a59.jpg"
+  },
+  {
+    "revision": "7325ad37fdc907e2d61512c479ef3897",
+    "url": "/updateduiux/static/media/filters grey.7325ad37.png"
+  },
+  {
+    "revision": "b2b74a39091b47ceba1b2c8c07ed1cd6",
+    "url": "/updateduiux/static/media/filters.b2b74a39.png"
+  },
+  {
+    "revision": "6858a3050ce5274f86956cf8cfe943f8",
+    "url": "/updateduiux/static/media/games gradient.6858a305.png"
+  },
+  {
+    "revision": "7ca28a618042d81f80a8f365175d73c5",
+    "url": "/updateduiux/static/media/games grey.7ca28a61.png"
+  },
+  {
+    "revision": "f96ff986e7b1b9e8cd7ab27bae93cddb",
+    "url": "/updateduiux/static/media/gaming colour.f96ff986.png"
+  },
+  {
+    "revision": "b8eadfe1107868d9540c0c44e512d530",
+    "url": "/updateduiux/static/media/gaming grey.b8eadfe1.png"
+  },
+  {
+    "revision": "4fddd023912d6fc81a8bf0cb040127ce",
+    "url": "/updateduiux/static/media/gaming.4fddd023.jpg"
+  },
+  {
+    "revision": "bec8c043343cfce099d1b985d718adc9",
+    "url": "/updateduiux/static/media/groove colour.bec8c043.png"
+  },
+  {
+    "revision": "b35810a17c85740b52d0787048552ffc",
+    "url": "/updateduiux/static/media/groove.b35810a1.png"
+  },
+  {
+    "revision": "c56564eda7fdba5ee2c442225bf5dd6a",
+    "url": "/updateduiux/static/media/guitar.c56564ed.png"
+  },
+  {
+    "revision": "069d249488a5986d8ceff26aceab8c9e",
+    "url": "/updateduiux/static/media/guitar2.069d2494.png"
+  },
+  {
+    "revision": "ae4956cfae4d0ead0c20c29ca860de4d",
+    "url": "/updateduiux/static/media/homef.ae4956cf.png"
+  },
+  {
+    "revision": "4d5e281360bf02492fe90d571f39cfef",
+    "url": "/updateduiux/static/media/homeo.4d5e2813.png"
+  },
+  {
+    "revision": "5ad8cd190e6422427f3e1db26c4e4b64",
+    "url": "/updateduiux/static/media/i3 grey.5ad8cd19.png"
+  },
+  {
+    "revision": "83c15999dde98917ed57f3bc566c5c44",
+    "url": "/updateduiux/static/media/i3colour.83c15999.png"
+  },
+  {
+    "revision": "e6b526c82a09a95e50ae55ce139e70ad",
+    "url": "/updateduiux/static/media/i5 colour.e6b526c8.png"
+  },
+  {
+    "revision": "c909b55765d8efdef7abfc17df020597",
+    "url": "/updateduiux/static/media/i5 grey.c909b557.png"
+  },
+  {
+    "revision": "94ff9c3900263803c12c6d26609de53d",
+    "url": "/updateduiux/static/media/i7 colour.94ff9c39.png"
+  },
+  {
+    "revision": "f568a5e3fecc1640cabd1dcd26994e71",
+    "url": "/updateduiux/static/media/i7 grey.f568a5e3.png"
+  },
+  {
+    "revision": "ce5b40e449699e56ee29cab1ef71faa1",
+    "url": "/updateduiux/static/media/keyboard grey.ce5b40e4.png"
+  },
+  {
+    "revision": "8eda4415bec99e3c1deb53f7d056f9c3",
+    "url": "/updateduiux/static/media/keyboard volour.8eda4415.png"
+  },
+  {
+    "revision": "ea53f6533e6716ecef12af87b1c06346",
+    "url": "/updateduiux/static/media/lens grey.ea53f653.png"
+  },
+  {
+    "revision": "58e21347b4a616efe381476949a206d5",
+    "url": "/updateduiux/static/media/lense color.58e21347.png"
+  },
+  {
+    "revision": "c97e73f5d5330c01929efd0d0c20d238",
+    "url": "/updateduiux/static/media/lights colour.c97e73f5.png"
+  },
+  {
+    "revision": "a9bd274faec68fa0d3068e74c09443a8",
+    "url": "/updateduiux/static/media/lights grey.a9bd274f.png"
+  },
+  {
+    "revision": "113abc5f81994d020e6623144220d55b",
+    "url": "/updateduiux/static/media/listing_g.113abc5f.png"
+  },
+  {
+    "revision": "c5b77b5de1bd836ba381784f64d1f279",
+    "url": "/updateduiux/static/media/listing_s.c5b77b5d.png"
+  },
+  {
+    "revision": "622a354e13ca19dd03bb68298e2de766",
+    "url": "/updateduiux/static/media/lock.622a354e.png"
+  },
+  {
+    "revision": "7d3d55f95d0cf5e4e4c6f3f5376596ea",
+    "url": "/updateduiux/static/media/logo.7d3d55f9.png"
+  },
+  {
+    "revision": "a636beae9ef3403a700964ae35a0cf98",
+    "url": "/updateduiux/static/media/logo_trans.a636beae.png"
+  },
+  {
+    "revision": "164a9c6c0dfe0c8a57faea8457754aef",
+    "url": "/updateduiux/static/media/macbook colour.164a9c6c.png"
+  },
+  {
+    "revision": "c2abdb5efa618dda4a8fdaeddcfacec9",
+    "url": "/updateduiux/static/media/macbook grey.c2abdb5e.png"
+  },
+  {
+    "revision": "e82877ea9e9f571e35bfcb76e4c47ef8",
+    "url": "/updateduiux/static/media/map.e82877ea.png"
+  },
+  {
+    "revision": "8511cbb55cf0a24e5fefc2b155a01c47",
+    "url": "/updateduiux/static/media/mumbai.8511cbb5.png"
+  },
+  {
+    "revision": "9b9b0741166960a0064c2c5f7052d0c8",
+    "url": "/updateduiux/static/media/mumbaig.9b9b0741.png"
+  },
+  {
+    "revision": "574b44e9bd91b07e4ed384233748d8bd",
+    "url": "/updateduiux/static/media/musical.574b44e9.jpg"
+  },
+  {
+    "revision": "14d711280d3a5c48c8d7a4ef4d25f23d",
+    "url": "/updateduiux/static/media/new icin-16.14d71128.png"
+  },
+  {
+    "revision": "b4f2ead0c1dbd031cbaf177a2e28ec92",
+    "url": "/updateduiux/static/media/packages colour.b4f2ead0.png"
+  },
+  {
+    "revision": "c436cc6a379b8cec7bb3b23711e9e9df",
+    "url": "/updateduiux/static/media/packages grey.c436cc6a.png"
+  },
+  {
+    "revision": "65174d12f45ae2f676657a5d7119a92d",
+    "url": "/updateduiux/static/media/pc colour.65174d12.png"
+  },
+  {
+    "revision": "1c0b119b830affb4b7dbbdd5eff5fcaf",
+    "url": "/updateduiux/static/media/pc gradient.1c0b119b.png"
+  },
+  {
+    "revision": "ad0c6f43c8ab41b559db0749494636cd",
+    "url": "/updateduiux/static/media/pc grey.ad0c6f43.png"
+  },
+  {
+    "revision": "f20504397a04974695faf532291a6a20",
+    "url": "/updateduiux/static/media/pc grey.f2050439.png"
+  },
+  {
+    "revision": "3b377f480d527e8e0328ef155a3478ef",
+    "url": "/updateduiux/static/media/pencil.3b377f48.png"
+  },
+  {
+    "revision": "fd87dde364937217720f195da6c57385",
+    "url": "/updateduiux/static/media/pencil_white.fd87dde3.png"
+  },
+  {
+    "revision": "9f945e1413856c9ba39249fb3dd9943c",
+    "url": "/updateduiux/static/media/percussion colour.9f945e14.png"
+  },
+  {
+    "revision": "7996e55528aed8e6678ad0acde70eace",
+    "url": "/updateduiux/static/media/percussion grey.7996e555.png"
+  },
+  {
+    "revision": "28583392e8acf190156b5d48aebeea8f",
+    "url": "/updateduiux/static/media/recording colour.28583392.png"
+  },
+  {
+    "revision": "86e1d245297736a403cecc4a79db9297",
+    "url": "/updateduiux/static/media/recording colour.86e1d245.png"
+  },
+  {
+    "revision": "c4153e2a487cfee50bf66676b087460b",
+    "url": "/updateduiux/static/media/recording grey.c4153e2a.png"
+  },
+  {
+    "revision": "b73201fec451a69840300e5f1431bfb6",
+    "url": "/updateduiux/static/media/recording.b73201fe.png"
+  },
+  {
+    "revision": "c09cad94642fae9fa44eb2a95284728c",
+    "url": "/updateduiux/static/media/rental_g.c09cad94.png"
+  },
+  {
+    "revision": "b228414c0146152d7871b880fe2ccecf",
+    "url": "/updateduiux/static/media/rental_s.b228414c.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/updateduiux/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/updateduiux/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/updateduiux/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "7f651b185b1f34d676c3be85f19d2923",
+    "url": "/updateduiux/static/media/rigs color.7f651b18.png"
+  },
+  {
+    "revision": "90dd57f828fdc66338742369081916fa",
+    "url": "/updateduiux/static/media/rigs grey.90dd57f8.png"
+  },
+  {
+    "revision": "d6dc0b554ce97185d0cef42f98d0cc14",
+    "url": "/updateduiux/static/media/search_boy.d6dc0b55.png"
+  },
+  {
+    "revision": "5779ec365fc4dbdcf1a4eb403bccc889",
+    "url": "/updateduiux/static/media/searchf.5779ec36.png"
+  },
+  {
+    "revision": "81a6185d5adca381492276db3062b2a1",
+    "url": "/updateduiux/static/media/searcho.81a6185d.png"
+  },
+  {
+    "revision": "339ea2932a758ecf3f2ca43acc17b088",
+    "url": "/updateduiux/static/media/selectedCategory1.339ea293.png"
+  },
+  {
+    "revision": "60e6e0dd551f8050568b110ca0b0126e",
+    "url": "/updateduiux/static/media/selectedCategory2.60e6e0dd.png"
+  },
+  {
+    "revision": "912404a8ec05af4118bf42f40ac46ed9",
+    "url": "/updateduiux/static/media/selectedCategory3.912404a8.png"
+  },
+  {
+    "revision": "9fedb29b07d9462e9cbecded0a873e72",
+    "url": "/updateduiux/static/media/selectedCategory4.9fedb29b.png"
+  },
+  {
+    "revision": "7f29ce37df14508528e7e987c9817a11",
+    "url": "/updateduiux/static/media/settings_g.7f29ce37.png"
+  },
+  {
+    "revision": "9c9a79e3f195ce0037d0e470f19ec7dc",
+    "url": "/updateduiux/static/media/settings_s.9c9a79e3.png"
+  },
+  {
+    "revision": "a4f0995223c763ed05e6fc09a99bf5e7",
+    "url": "/updateduiux/static/media/share.a4f09952.png"
+  },
+  {
+    "revision": "5641838f5aacbe447f92328db71da3d2",
+    "url": "/updateduiux/static/media/shareCredit_g.5641838f.png"
+  },
+  {
+    "revision": "0cfa551165162b068dff511363cf042c",
+    "url": "/updateduiux/static/media/shareCredit_s.0cfa5511.png"
+  },
+  {
+    "revision": "44b2f1a1e674676aebbc5fbeab88fdd1",
+    "url": "/updateduiux/static/media/slide1.44b2f1a1.jpg"
+  },
+  {
+    "revision": "aa9395f25ae4643f55f6c67a637050c7",
+    "url": "/updateduiux/static/media/slide2.aa9395f2.jpg"
+  },
+  {
+    "revision": "e17bb92254323d437aa5ab429df2287c",
+    "url": "/updateduiux/static/media/slide3.e17bb922.jpg"
+  },
+  {
+    "revision": "9ca461d5f21556e3c2a0df56c42af3d5",
+    "url": "/updateduiux/static/media/support_g.9ca461d5.png"
+  },
+  {
+    "revision": "70f0f5bd1fe4bb387db375551a4cfc27",
+    "url": "/updateduiux/static/media/support_s.70f0f5bd.png"
+  },
+  {
+    "revision": "9ca7b8632422265c606cf735ecb7ff7e",
+    "url": "/updateduiux/static/media/tablet color.9ca7b863.png"
+  },
+  {
+    "revision": "ac72f84900f3d6955c7f897140950460",
+    "url": "/updateduiux/static/media/tablet grey.ac72f849.png"
+  },
+  {
+    "revision": "855265d615f99dbb0fb0cbbe4f6823d5",
+    "url": "/updateduiux/static/media/tick_circle.855265d6.png"
+  },
+  {
+    "revision": "c249527f9d33f843b1095cf1ad059dcf",
+    "url": "/updateduiux/static/media/verification_g.c249527f.png"
+  },
+  {
+    "revision": "712cde71e66522831a42c6349ae83152",
+    "url": "/updateduiux/static/media/verification_s.712cde71.png"
+  },
+  {
+    "revision": "e6fcd0e0fca89bd606ced01eefbcca5a",
+    "url": "/updateduiux/static/media/wave.e6fcd0e0.png"
+  },
+  {
+    "revision": "dc8077e731004aef10c2395cf2f4dfd7",
+    "url": "/updateduiux/static/media/wind color.dc8077e7.png"
+  },
+  {
+    "revision": "2b7fb30c1232a94d37c49b1920a5e22d",
+    "url": "/updateduiux/static/media/wind grey.2b7fb30c.png"
+  }
+]);
