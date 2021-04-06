f0a7c6f16882a6d7505b801b1401525f8a9a03e7
Rentkar <rentkarr@gmail.com>
2021-04-06 11:30:42 +0530

    uploadproduct

--- src/components/admin/AllProducts.js
+++ src/components/admin/AllProducts.js
@@ -1,11 +1,17 @@
 import React, { Component, useState, useContext, useEffect } from "react";
-import { Table} from 'react-bootstrap'
-import { DropzoneArea } from 'material-ui-dropzone';
-import {Modal} from 'react-bootstrap'
+import { Table } from "react-bootstrap";
+import { DropzoneArea } from "material-ui-dropzone";
+import { Modal } from "react-bootstrap";
 import "./admindashboard.css";
-import EditProductDetails from './editproductdetails'
-import { fetchProducts, fetchUsers, fetchSupport, fetchOrderReq, fetchLenderReq} from '../../config'
-import axios from "axios"
+import EditProductDetails from "./editproductdetails";
+import {
+  fetchProducts,
+  fetchUsers,
+  fetchSupport,
+  fetchOrderReq,
+  fetchLenderReq,
+} from "../../config";
+import axios from "axios";
 import {
   Select,
   TextField,
@@ -19,23 +25,20 @@ import {
   Switch,
   Route,
   Link,
-  withRouter
+  withRouter,
 } from "react-router-dom";
 
-
-export function AllProducts ()
-{
-const [ p, setP ] = useState( [] )
-    useEffect(() => {
+export function AllProducts() {
+  const [p, setP] = useState([]);
+  useEffect(() => {
     const fetchAPI = async () => {
-        setP( await fetchProducts() );
+      setP(await fetchProducts());
     };
-	fetchAPI();
-	},
-      [] );
+    fetchAPI();
+  }, []);
 
-	const useStyles = makeStyles( ( theme ) => ( {
-	  categoryInput: {
+  const useStyles = makeStyles((theme) => ({
+    categoryInput: {
       paddingTop: "0px",
       width: "100%",
     },
@@ -44,79 +47,71 @@ const [ p, setP ] = useState( [] )
       minWidth: 120,
     },
   }));
-  
-  
-  function AddProductModal ( props )
-  {
+
+  function AddProductModal(props) {
     const classes = useStyles();
     const [category, setCategories] = useState("category");
-    const [subcat, setSubcat] = useState([]);
+    const [subcat, setSubcat] = useState();
     const [fields, setFields] = useState([{ value: null }]);
     const [name, setName] = useState("");
-    const [imagepath, setImagePath] = useState([]);
-    
-    const userForm = ( e ) =>
-    {
-      e.preventDefault()
-      axios.post( 'URL', 'datafile' ) // post url and datafile
-        .then( function ( response )
-        {
-        console.log(response)
-      })
-    }
-    const handleChange = ( event ) =>
-    {
-      setCategories(event.target.value)
+    const [imagepath, setImagePath] = useState();
+
+    const userForm = (e) => {
+      e.preventDefault();
+      axios
+        .post("URL", "datafile") // post url and datafile
+        .then(function(response) {
+          console.log(response);
+        });
+    };
+    const handleChange = (event) => {
+      setCategories(event.target.value);
+    };
+
+    const subChange = (event) => {
+      setSubcat(event.target.value);
+    };
+
+    function handleChanges(i, event) {
+      const values = [...fields];
+      values[i].value = event.target.value;
+      setFields(values);
     }
 
-    const subChange = ( event ) =>
-    {
-      setSubcat( event.target.value)
+    function handleAdd() {
+      const values = [...fields];
+      values.push({ value: null });
+      setFields(values);
+      console.log("add event specifications");
     }
 
-    function handleChanges ( i, event )
-    {
-      const values = [ ...fields ]
-      values[ i ].value = event.target.value
-      setFields(values)
+    function handleRemove(i) {
+      const values = [...fields];
+      values.splice(i, 1);
+      setFields(values);
     }
 
-    function handleAdd ()
-    {
-      const values = [ ...fields ]
-      values.push( { value: null } )
-      setFields( values )
-      console.log('add event specifications')
-    }
-
-    function handleRemove ( i )
-    {
-      const values = [ ...fields ]
-      values.splice( i, 1 )
-      setFields(values)
-	  }
-	  
-	    function onSubmit(e) {
+    function onSubmit(e) {
       e.preventDefault();
       console.log(name);
       axios.post("http://localhost:5000/products", {
         name: name,
-        images: "",
-        category: "",
-        subcategory: "",
-        quantity: "",
-        pricing: "",
-        featured: "",
-        specifiction: "",
-        limitedStock: "",
-        availableInMumbai: "",
-        availableInPune: "",
-        box: "",
-        adOns: "",
+        images: imagepath,
+        category: category,
+        subcategory: subcat,
+        quantity: null,
+        pricing: null,
+        featured: null,
+        specifiction: null,
+        limitedStock: null,
+        availableInMumbai: null,
+        availableInPune: null,
+        box: null,
+        adOns: null,
       });
     }
-    
-     return (
+
+    return (
       <Modal
         {...props}
         size="lg"
@@ -129,9 +124,10 @@ const [ p, setP ] = useState( [] )
         <Modal.Body>
           <form className={classes.formControl} onSubmit={(e) => onSubmit(e)}>
             <DropzoneArea
-          //onChange={this.handleChange.bind(this)}
+              //onChange={this.handleChange.bind(this)}
+              onchange={(e) => setImagePath(e.target.value)}
             />
-             <TextField
+            <TextField
               label="Name"
               onChange={(e) => setName(e.target.value)}
               value={name}
@@ -189,10 +185,10 @@ const [ p, setP ] = useState( [] )
                     value={category}
                     onChange={handleChange}
                   >
-                    <MenuItem value={10}>MUSIC</MenuItem>
-                    <MenuItem value={20}>GAMING</MenuItem>
-                    <MenuItem value={30}>LAPTOP</MenuItem>
-                    <MenuItem value={40}>PHOTOGRAPHY</MenuItem>
+                    <MenuItem value="MUSIC">MUSIC</MenuItem>
+                    <MenuItem value="GAMING">GAMING</MenuItem>
+                    <MenuItem value="LAPTOP">LAPTOP</MenuItem>
+                    <MenuItem value="PHOTOGRAPHY">PHOTOGRAPHY</MenuItem>
                   </Select>
                 </div>
 
@@ -203,27 +199,27 @@ const [ p, setP ] = useState( [] )
                     value={subcat}
                     onChange={subChange}
                   >
-                    <MenuItem value={10}>GUITAR</MenuItem>
-                    <MenuItem value={20}>KEYBOARD</MenuItem>
-                    <MenuItem value={30}>PERCUSSION</MenuItem>
-                    <MenuItem value={40}>RECORDING</MenuItem>
-                    <MenuItem value={50}>PACKAGES</MenuItem>
-                    <MenuItem value={60}>PC GAMING</MenuItem>
-                    <MenuItem value={70}>CONSOLE</MenuItem>
-                    <MenuItem value={80}>ACCESSORIES</MenuItem>
-                    <MenuItem value={90}>GAMING</MenuItem>
-                    <MenuItem value={100}>i3</MenuItem>
-                    <MenuItem value={11}>i5</MenuItem>
-                    <MenuItem value={12}>i7</MenuItem>
-                    <MenuItem value={13}>MACBOOK</MenuItem>
-                    <MenuItem value={14}>PC</MenuItem>
-                    <MenuItem value={15}>TABLETS</MenuItem>
-                    <MenuItem value={16}>CAMERA</MenuItem>
-                    <MenuItem value={17}>FILTERS</MenuItem>
-                    <MenuItem value={18}>LENS</MenuItem>
-                    <MenuItem value={19}>LIGHTS</MenuItem>
-                    <MenuItem value={20}>RECORDING</MenuItem>
-                    <MenuItem value={21}>RIGS</MenuItem>
+                    <MenuItem value="GUITAR">GUITAR</MenuItem>
+                    <MenuItem value="KEYBOARD">KEYBOARD</MenuItem>
+                    <MenuItem value="PERCUSSION">PERCUSSION</MenuItem>
+                    <MenuItem value="RECORDING">RECORDING</MenuItem>
+                    <MenuItem value="PACKAGES">PACKAGES</MenuItem>
+                    <MenuItem value="PC GAMING">PC GAMING</MenuItem>
+                    <MenuItem value="CONSOLE">CONSOLE</MenuItem>
+                    <MenuItem value="ACCESSORIES">ACCESSORIES</MenuItem>
+                    <MenuItem value="GAMING">GAMING</MenuItem>
+                    <MenuItem value="i3">i3</MenuItem>
+                    <MenuItem value="i5">i5</MenuItem>
+                    <MenuItem value="i7">i7</MenuItem>
+                    <MenuItem value="MACBOOK">MACBOOK</MenuItem>
+                    <MenuItem value="PC">PC</MenuItem>
+                    <MenuItem value="TABLETS">TABLETS</MenuItem>
+                    <MenuItem value="CAMERA">CAMERA</MenuItem>
+                    <MenuItem value="FILTERS">FILTERS</MenuItem>
+                    <MenuItem value="LENS">LENS</MenuItem>
+                    <MenuItem value="LIGHTS">LIGHTS</MenuItem>
+                    <MenuItem value="RECORDING">RECORDING</MenuItem>
+                    <MenuItem value="RIGS">RIGS</MenuItem>
                   </Select>
                 </div>
               </div>
@@ -246,7 +242,7 @@ const [ p, setP ] = useState( [] )
               style={{ width: "100%", padding: "1%" }}
               placeholder="Description"
             />
-             {/*    <label>Specification</label>
+            {/*    <label>Specification</label>
             <div>
               {/* <TextField label="Brand" />
               <TextField label="Specification" /> 
@@ -409,74 +405,93 @@ const [ p, setP ] = useState( [] )
                 Add a new Add On
               </Btn>
             </div> */}
-             <DropzoneArea
-          //onChange={this.handleChange.bind(this)}
+            <DropzoneArea
+            //onChange={this.handleChange.bind(this)}
             />
-            <input className='btn btn-outline-success submit' type="submit" value="submit" />
+            <input
+              className="btn btn-outline-success submit"
+              type="submit"
+              value="submit"
+            />
           </form>
         </Modal.Body>
       </Modal>
     );
   }
-  
-  
-  function Products ()
-  {
-        const [ addProductModalShow, setAddProductModalShow ] = useState( false ) 
 
-  return (
-    <div className='allproducts'>
-            <div className='buttons'>
-              <div className= 'btn btn-outline-success'>FILTER <i className="fas fa-chevron-circle-down" /></div>
-        <div className='btn btn-outline-info' onClick={ () => setAddProductModalShow( true ) }><i className="fas fa-plus-circle" /> ADD A NEW PRODUCT</div>
-        <AddProductModal show={addProductModalShow} onHide={()=>setAddProductModalShow(false)} />
-            </div>
+  function Products() {
+    const [addProductModalShow, setAddProductModalShow] = useState(false);
 
+    return (
+      <div className="allproducts">
+        <div className="buttons">
+          <div className="btn btn-outline-success">
+            FILTER <i className="fas fa-chevron-circle-down" />
+          </div>
+          <div
+            className="btn btn-outline-info"
+            onClick={() => setAddProductModalShow(true)}
+          >
+            <i className="fas fa-plus-circle" /> ADD A NEW PRODUCT
+          </div>
+          <AddProductModal
+            show={addProductModalShow}
+            onHide={() => setAddProductModalShow(false)}
+          />
+        </div>
 
-        <div className='table'>
-        <Table striped bordered hover responsive className="table-sm">
-          <thead>
-            <th>_id</th>
-            <th>PRODUCT ID</th>
-            <th>CATEGORY</th>
-            <th>SUB CATEGORY</th>
-            <th>NAME</th>
-            <th>LENDERS</th>
-            <th>EDIT</th>
-          </thead>
-          <tbody>
-        { p.map( ( item, index ) =>
-        {
-          return (
-            <tr>
-              <td>{item._id}</td>
-              <td>{item.id}</td>
-              <td>{item.category}</td>
-              <td>{item.subcategory}</td>
-              <td>{item.productName}</td>
-              <td><Link to='/alllenders' ><i className="fas fa-info" /></Link></td>
-              <td><Link to={ `/allproducts/editproductdetails/${ item._id }` }><i className="fas fa-edit" /></Link></td>
-            </tr>
-          )
-        })}
-          </tbody>
-        </Table>
-      
-      </div>  
-  </div>
-    )
+        <div className="table">
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <th>_id</th>
+              <th>PRODUCT ID</th>
+              <th>CATEGORY</th>
+              <th>SUB CATEGORY</th>
+              <th>NAME</th>
+              <th>LENDERS</th>
+              <th>EDIT</th>
+            </thead>
+            <tbody>
+              {p.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{item._id}</td>
+                    <td>{item.id}</td>
+                    <td>{item.category}</td>
+                    <td>{item.subcategory}</td>
+                    <td>{item.productName}</td>
+                    <td>
+                      <Link to="/alllenders">
+                        <i className="fas fa-info" />
+                      </Link>
+                    </td>
+                    <td>
+                      <Link to={`/allproducts/editproductdetails/${item._id}`}>
+                        <i className="fas fa-edit" />
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
   }
 
-
-  
   return (
     <Router>
       <div>
         <Switch>
-          <Route path='/allproducts/editproductdetails/:_id'><EditProductDetails /></Route>
-          <Route path='/allproducts/'><Products /></Route>
+          <Route path="/allproducts/editproductdetails/:_id">
+            <EditProductDetails />
+          </Route>
+          <Route path="/allproducts/">
+            <Products />
+          </Route>
         </Switch>
-      </div>  
+      </div>
     </Router>
-  )
+  );
 }
